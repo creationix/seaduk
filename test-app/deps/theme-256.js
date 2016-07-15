21d69f9e1bababb3f990663800c4ea71bccd4f40
Tim Caswell <tim@creationix.com>
2016-07-15 16:05:44 -0500

    Simplify sample theme format

--- test-app/deps/theme-256.js
+++ test-app/deps/theme-256.js
@@ -1,36 +1,30 @@
-(function(){
-"use strict";
-
 // nice color theme using 256-mode colors
-var theme = {};
+({
+  property:  "38;5;253",
+  braces:    "38;5;247",
+  sep:       "38;5;240",
 
-theme.property  = "38;5;253";
-theme.braces    = "38;5;247";
-theme.sep       = "38;5;240";
+  undefined: "38;5;244",
+  boolean:   "38;5;220", // yellow-orange
+  number:    "38;5;202", // orange
+  string:    "38;5;34",  // darker green
+  quotes:    "38;5;40",  // green
+  escape:    "38;5;46",  // bright green
+  function:  "38;5;129", // purple
+  cfunction: "38;5;161", // purple-red
+  thread:    "38;5;199", // pink
 
-theme.undefined = "38;5;244";
-theme.boolean   = "38;5;220"; // yellow-orange
-theme.number    = "38;5;202"; // orange
-theme.string    = "38;5;34";  // darker green
-theme.quotes    = "38;5;40";  // green
-theme.escape    = "38;5;46";  // bright green
-theme.function  = "38;5;129"; // purple
-theme.cfunction = "38;5;161"; // purple-red
-theme.thread    = "38;5;199"; // pink
+  regexp:    "38;5;214", // yellow-orange
+  date:      "38;5;153", // blue-purple
 
-theme.regexp    = "38;5;214"; // yellow-orange
-theme.date      = "38;5;153"; // blue-purple
+  null:      "38;5;27",  // dark blue
+  object:    "38;5;27",  // blue
+  buffer:    "38;5;39",  // blue2
+  dbuffer:   "38;5;69",  // teal
+  pointer:   "38;5;124", // red
 
-theme.null      = "38;5;27";  // dark blue
-theme.object    = "38;5;27";  // blue
-theme.buffer    = "38;5;39";  // blue2
-theme.dbuffer   = "38;5;69";  // teal
-theme.pointer   = "38;5;124"; // red
-
-theme.error     = "38;5;196"; // bright red
-theme.success   = "38;5;120;48;5;22";  // bright green
-theme.failure   = "38;5;215;48;5;52";  // bright green
-theme.highlight = "38;5;45;48;5;236";  // bright teal with grey background
-
-return theme;
-})();
+  error:     "38;5;196", // bright red
+  success:   "38;5;120;48;5;22",  // bright green
+  failure:   "38;5;215;48;5;52",  // bright green
+  highlight: "38;5;45;48;5;236",  // bright teal with grey background
+})
