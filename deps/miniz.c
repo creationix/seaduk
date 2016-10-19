6318f5d5d1cafcc5ab4734dc49fe899807cfb229
Tim Caswell <tim@creationix.com>
2016-10-18 23:00:32 -0500

    Ignore unused param in miniz

--- deps/miniz.c
+++ deps/miniz.c
@@ -1059,7 +1059,7 @@ int mz_deflateEnd(mz_streamp pStream)
 
 mz_ulong mz_deflateBound(mz_streamp pStream, mz_ulong source_len)
 {
-  pStream;
+  (void)(pStream);
   // This is really over conservative. (And lame, but it's actually pretty tricky to compute a true upper bound given the way tdefl's blocking works.)
   return MZ_MAX(128 + (source_len * 110) / 100, 128 + source_len + ((source_len / (31 * 1024)) + 1) * 5);
 }
